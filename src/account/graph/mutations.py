174ab0225a9c4a6934c4783ae94c065e7b709272
Carol <awdrg1210@gmail.com>
2024-10-15 17:17:46 +0800

    feat(user): add ConflictError exception for create user mutation

--- src/account/graph/mutations.py
+++ src/account/graph/mutations.py
@@ -3,10 +3,24 @@ import strawberry_django
 
 from account.graph import types as UserTypes
 from account.graph.services import UserService
+from utils import exceptions as exc
+from utils.constants import CustomErrorCode
+
+
+@strawberry.type
+class UserMutations:
+
+    @strawberry_django.mutation
+    async def create(self, user_in: UserTypes.UserCreate) -> UserTypes.User:
+        user = await UserService.get_user(email=user_in.email)
+        if user:
+            raise exc.ConflictError(CustomErrorCode.ENTITY_CONFLICT, "Email already registered")
+        return await UserService.create_user(obj_in=user_in)
 
 
 @strawberry.type
 class Mutation:
-    @strawberry_django.mutation
-    async def create(self, user_in: UserTypes.UserCreate) -> UserTypes.User:
-        return await UserService.create_user(obj_in=user_in)
+
+    @strawberry.field
+    def user(self) -> UserMutations:
+        return UserMutations()
